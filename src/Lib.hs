487aa892de1fd159016225b46db4a3dfbe1a99a1
Saurabh M. Machave <saurabmm@thoughtworks.com>
2018-07-06 20:31:11 +0530

    added code to create vertical slices of movesˆ

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,60 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib (
+    Board(..),
+    hasWon
+) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Foldable
+
+data BoardGridPlaceholder = Player1 | Player2 | Empty deriving (Eq)
+
+instance Show BoardGridPlaceholder where
+    show Player1 = "X"
+    show Player2 = "O"
+    show Empty = ""
+
+newtype BoardGrid = BoardGrid [[BoardGridPlaceholder]] deriving (Eq, Show)
+--
+-- data BoardConfig = BoardConfig {
+--     horizontalSlots :: Int,
+--     verticalSlots :: Int
+-- } deriving (Eq, Show)
+
+newtype Board = Board {
+--     config :: BoardConfig,
+    moves :: [Int]
+--     grid :: BoardGrid
+} deriving (Eq, Show)
+
+getPlayerBasedOnCount :: (Num a, Integral a) => a -> BoardGridPlaceholder
+getPlayerBasedOnCount iterationCount
+    | even iterationCount = Player1
+    | otherwise = Player2
+
+generateSliceMap :: (Num a, Eq a, Integral a) => [a] -> a -> [[BoardGridPlaceholder]]
+generateSliceMap subArray count = let boardGrid = [] in case subArray of
+    [] -> boardGrid
+    (x:xs) -> [ if place == x then
+                    getPlayerBasedOnCount count
+                else
+                    Empty
+                | place <- [1..7]
+              ] : generateSliceMap xs (count + 1)
+
+squashSlicesInnerLoop :: Int -> [[BoardGridPlaceholder]] -> [BoardGridPlaceholder]
+squashSlicesInnerLoop iteration arrayOfArrays =
+    foldl (\placeHolders placeHolder ->
+        if placeHolder == Empty then placeHolders else placeHolders ++ [placeHolder]
+    ) [] $
+        foldl (\captureArray sliceIndex ->
+            ((arrayOfArrays !! sliceIndex) !! iteration) : captureArray) [] [0..(length arrayOfArrays - 1)]
+
+squashSlices :: [[BoardGridPlaceholder]] -> [[BoardGridPlaceholder]]
+squashSlices arrayOfArrays = foldl (\accumulator iteration ->
+        squashSlicesInnerLoop iteration arrayOfArrays : accumulator
+    ) [] [0..6]
+
+makeBoardGrid :: Board -> BoardGrid
+makeBoardGrid (Board array) = BoardGrid $ squashSlices $ generateSliceMap array 0
+
+hasWon :: Board -> Bool
+hasWon b = True
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,9 @@
+import Test.Hspec
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+    describe "Connect4 Logic" $
+        it "should win when identical colour is in 4 consecutive rows" $
+            hasWon $ Board [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]
