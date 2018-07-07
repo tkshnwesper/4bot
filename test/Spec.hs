fbeb75d3b22bc8bc39a1e732d8f0eb030e39b201
Saurabh M. Machave <saurabmm@thoughtworks.com>
2018-07-07 13:04:06 +0530

    passing checks for verticalˆ

--- test/Spec.hs
+++ test/Spec.hs
@@ -4,6 +4,8 @@ import Lib
 
 main :: IO ()
 main = hspec $
-    describe "Connect4 Logic" $
-        it "should win when identical colour is in 4 consecutive rows" $
+    describe "Connect4 Logic" $ do
+        it "should win when identical colour is in 4 consecutive columns" $
             hasWon $ Board [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]
+        it "should win when identical colour is in 4 consecutive rows" $
+            hasWon $ Board [1, 2, 1, 3, 1, 4, 1]
