715afe8877916cc579451617b3303ae6ee0d20d7
Saurabh M. Machave <saurabmm@thoughtworks.com>
2018-07-09 14:52:47 +0530

    writing tests for displaying boardˆ

--- app/Main.hs
+++ app/Main.hs
@@ -5,5 +5,4 @@ import Lib
 
 main :: IO ()
 main =
-    let (BoardGrid array) = makeBoardGrid $ Board [4, 3, 3, 2, 1, 2, 2, 1, 1, 5, 1] in
-    print $ BoardGrid (reverse . transpose $ array)
+    print (makeBoardGrid $ Board [1])
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,7 @@ import Test.Hspec
 import Lib
 
 main :: IO ()
-main = hspec $
+main = hspec $ do
     describe "Connect4 Logic" $ do
         it "should win when identical colour is in 4 consecutive columns [Player 1]" $
             hasWon $ Board [0, 0, 1, 1, 2, 2, 3, 3, 4, 4]
@@ -21,3 +21,11 @@ main = hspec $
             hasWon $ Board [4, 3, 3, 2, 1, 2, 2, 1, 1, 5, 1]
         it "should win when four identical colours are in the opposite diagonal consecutively [Player 2]" $
             hasWon $ Board [6, 4, 3, 3, 2, 1, 2, 2, 1, 1, 5, 1]
+
+    describe "Displaying Board" $ do
+        it "should print nothing when no moves have been made" $
+            show (makeBoardGrid $ Board []) == ""
+        it "should print X followed by _ when first move has been made in first column" $
+            show (makeBoardGrid $ Board [1]) == "X _ _ _ _ _ _ "
+        it "should print X O followed by _ when the second move has been made in second slot" $
+            show (makeBoardGrid $ Board [1, 2]) == "X O _ _ _ _ _ "
